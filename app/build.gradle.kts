138c02e7d8bccdde0269f80a8955a21f4d4e81c4
mango <Tunctas@proton.me>
2025-10-06 22:42:08 +0300

    logo made by https://github.com/mugagaz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.mang0.mindcleardemo"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "mangist"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
